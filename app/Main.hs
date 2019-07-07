765eeeb88db5647858b5e45e8d8cc4b82624d4a7
Antti Kukko <>
2019-07-07 19:43:08 +0300

    Show playoff stage name above the playoff matches

--- app/Main.hs
+++ app/Main.hs
@@ -41,10 +41,10 @@ main = do
 
     let longest = length (maximumBy (comparing length) (concat groups))
 
-    file <- readFile scheduleFile    
+    file <- readFile scheduleFile
 
     let results = readResults file
-    let prettySchedule = printableSchedule longest results 
+    let prettySchedule = printableSchedule longest results
 
     let scores = buildScoreboards results
     let prettyScores = printableScoreboards longest scores
@@ -58,10 +58,10 @@ main = do
     let playoffTeams = getPlayoffTeams advance scores
     putStrLn "Playoff teams:"
     putStrLn $ head $ printableScoreboards longest [playoffTeams]
-    
+
     playoffsExist <- doesFileExist playoffFile
 
-    let calculatedBrackets = getBrackets $ getFirstRound advance scores
+    let calculatedBrackets = getBrackets $ getFirstRound advance scores
 
     oldBrackets <- if playoffsExist
         then do
@@ -78,8 +78,18 @@ main = do
 
     writeSchedule playoffFile brackets
 
+    putStrLn ""
+
+    -- Build the final, semifinal etc. titles that go above the playoff results.
+    let teamAmounts = take (length brackets) (tail $ iterate (*2) 1)
+    let playoffStages = map (\x -> "Round of " ++ show x) teamAmounts
+    let withLine = map (\s -> s ++ "\n" ++ replicate (length s) '=') (addFinalNames playoffStages)
+    
+
     let playoffGames = printableSchedule longest brackets
-    putStrLn (intercalate "" playoffGames)
+    putStrLn (intercalate "\n\n" $ combine "" withLine playoffGames)
+
+    putStrLn ""
 
 
 getShuffled ts 0 = return ts
@@ -98,7 +108,7 @@ readInfo args = do
     ts <- readFile teamPath
     let teams = readTeams $ lines ts
     evaluate (force teams)
-    
+
     max <- if newGen
         then return (read (head args) :: Int)
         else do
@@ -119,3 +129,14 @@ readInfo args = do
         writeMaxTeamAmount configFile max
 
     return (groups, schedule)
+
+addFinalNames stages
+    | l > 2 = reverse $ f:sf:qf:drop 3 stages
+    | l > 1 = reverse $ f:sf:drop 2 stages
+    | l > 0 = reverse $ f:drop 1 stages
+    | otherwise = stages
+    where
+        l = length stages
+        f = "Final"
+        sf = "Semifinals"
+        qf = "Quarterfinals"
